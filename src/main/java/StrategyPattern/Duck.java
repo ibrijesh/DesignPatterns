3bf828abae88981c7a497e7590ef55b113ef4475
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-02-20 10:11:18 +0530

    But now we need the ducks to FLY
    - all the classes that inherit will have fly() method.

--- src/main/java/StrategyPattern/Duck.java
+++ src/main/java/StrategyPattern/Duck.java
@@ -2,9 +2,11 @@ package StrategyPattern;
 
 abstract class Duck {
 
-    void quack(){}
+    void quack() {}
 
     void swim() {}
+
+    void fly() {}
 
     abstract void display();      // abstract because since all the  duck subtype look's different.
 
