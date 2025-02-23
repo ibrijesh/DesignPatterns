572bcae1ea2d2e20a77eebb9475d9b9aae2557a4
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-02-23 11:26:36 +0530

    ForeCast Display Screen Added

---
+++ src/main/java/ObserverPattern/ForeCastDisplay.java
@@ -0,0 +1,30 @@
+package ObserverPattern;
+
+public class ForeCastDisplay implements Observer, DisplayElement {
+    private double maxTemperature;
+    private double maxHumidity;
+    private double maxPressure;
+    private WeatherData weatherData;
+
+    ForeCastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(final double temperature, final double humidity, final double pressure) {
+        this.maxTemperature = Math.max(temperature, this.maxTemperature);
+        this.maxHumidity = Math.max(humidity, this.maxHumidity);
+        this.maxPressure = Math.max(pressure, this.maxPressure);
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("ForeCastDisplay: {" +
+                "maxTemperature=" + maxTemperature +
+                ", maxHumidity=" + maxHumidity +
+                ", maxPressure=" + maxPressure +
+                '}');
+    }
+}
--- src/main/java/ObserverPattern/MainWeather.java
+++ src/main/java/ObserverPattern/MainWeather.java
@@ -6,6 +6,7 @@ public class MainWeather {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+        ForeCastDisplay foreCastDisplay = new ForeCastDisplay(weatherData);
 
         weatherData.setMeasurement(22.80, 45.55, 70.34);
         weatherData.setMeasurement(34.60, 40.20, 56.90);
